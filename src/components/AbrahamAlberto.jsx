ad000c76f15b7471182f3c1928fc14bf0d2ac42f
Abraham Alberto Queccano Gallegos <abrahamalbertoqueccanogallegos@MacBook-Pro.local>
2023-12-07 22:36:56 -0500

    no

--- src/components/AbrahamAlberto.jsx
+++ src/components/AbrahamAlberto.jsx
@@ -1,6 +1,14 @@
 const AbrahamAlberto = () => {
   return (
-    <div>AbrahamAlberto</div>
+    <article className="text-center p-4">
+      <header className="w-[250px] shadow-md rounded-md overflow-hidden">
+        <img src="/yo.jpeg" alt="" />
+      </header>
+      <h2 className="font-bold">AbrahamAlberto</h2>
+      <p className="text-slide-500 font-semibold">
+        Estudiante de Academlo
+      </p>
+    </article>
   )
 }
 export default abrahamAlberto
