6c0b4c8af18abb9acf6bfb5d18814e92a411ad5e
DESKTOP-IQMMSPN\ELIAS MARTINEZ <elmaf1990@gmail.com>
2023-12-07 22:39:56 -0500

    Actualizacion

--- src/components/EliasMartinez.jsx
+++ src/components/EliasMartinez.jsx
@@ -1,6 +1,12 @@
 const EliasMartinez = () => {
   return (
-    <div>EliasMartinez</div>
+    <article className="text-center">
+      <header className="[230px] shadow-lg rounded-md overflow-hidden mx-auto" >
+        <img src="/EliasMF.jpg" alt="" />
+      </header>
+      <h1 className="font-bold text-xl py-2">Elias MArtinez Funes</h1>
+      <p className="text-slate-500 font-semibold">Estudiante Academlo</p>
+    </article>
   )
 }
 export default EliasMartinez
