d1e342e2ba90ac137698f5fe666c6e2516f5535d
Jesús Aguilar <aguilaraliagajesus@gmail.com>
2023-12-07 23:45:46 -0400

    añadidido un componente nuevo de mi, y corregido algunos errores de main jsx, y renderizado de mi componente en el app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
+import JesusAguilar from "./components/JesusAguilar";
 function App(){
   return (
     <main >
-
+      <JesusAguilar />
     </main>
   );
-};
+}
 
 export default App;
 
